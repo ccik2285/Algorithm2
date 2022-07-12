bed5e9533d4a263f56134b66f7d03c224639aeab
lch <ccik1653@gmail.com>
2022-07-12 15:20:38 +0900

    .

--- 프로그래머스/튜프.cpp
+++ 프로그래머스/튜프.cpp
@@ -2,36 +2,31 @@
 #include <vector>
 #include <set>
 #include <algorithm>
-
+#include <iostream>
 using namespace std;
-
+vector<int> temp;
+vector<pair<int,vector<int>>> current;
 vector<int> solution(string s) {
-	vector<int> answer;
-	int num = 0;
-	vector<int> temp;
-	vector<pair<int, vector<int>>> current;
-	for (int i = 2; i < s.size() - 1; i++) {
-		if (s[i] - '0' >= 0 && s[i] - '0' < 10) {
-			num *= 10;
-			num += (s[i] - '0');
-		}
-		else if (s[i] == ',') {
-			if (s[i - 1] == '}') {
-				continue;
-			}
-			temp.push_back(num);
-			num = 0;
-		}
-		else if (s[i] == '}') {
-			temp.push_back(num);
+    vector<int> answer;
+    string a = "";
+    for(int i=2; i < s.size()-1; i++){
+        if(s[i] - '0' >= 0 && s[i] - '0' < 10){
+            a +=s[i];
+        }
+        else if(s[i] == ','){
+            if(s[i-1] == '}') continue;
+            temp.push_back(stoi(a));
+            a = "";
+        }
+        else if (s[i] == '}') {
+			temp.push_back(stoi(a));
 			current.push_back(make_pair(temp.size(), temp));
-			num = 0;
+			a = "";
 			temp.clear();
 		}
-	}
-	sort(current.begin(), current.end());
-
-	set<int> res;
+    }
+    sort(current.begin(), current.end());
+    set<int> res;
 	for (pair<int, vector<int>> p : current) {
 		for (int num : p.second) {
 			if (res.find(num) == res.end()) {
@@ -40,5 +35,5 @@ vector<int> solution(string s) {
 			}
 		}
 	}
-	return answer;
+    return answer;
 }
