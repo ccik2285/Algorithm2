d37f333b2ac19c6226c0a2e508bf65424fae5eb5
lch <ccik1653@gmail.com>
2022-07-19 19:27:22 +0900

    .

--- 프로그래머스/수식최대화.cpp
+++ 프로그래머스/수식최대화.cpp
@@ -1,55 +1,52 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
-
-long long calc(long long a,long long b, char op)
-{
-    if(op == '-')   return a-b;
-    else if(op == '+')  return a+b;
-    else    return a*b;
+long long calc(long long a,long long b,char op) {
+    if(op == '+') return a+b;
+    else if(op == '-') return a-b;
+    else return a*b;
 }
-
 long long solution(string expression) {
     long long answer = 0;
-    vector <char> oper_list =  { '*', '+', '-' };
-    vector <long long> number;
-    vector <char> oper;
-    string num = "";
-    
-
-    for(int i=0;i<expression.size();i++)
-    {
-        if(expression[i] == '+' || expression[i] =='*' || expression[i] == '-')
+    vector<char> op;
+    vector<long long> num;
+    vector<char> oper = {'*','+','-'};
+    string temp = "";
+    for(int i=0; i < expression.size(); i++){
+        if(expression[i] == '*' || expression[i] == '-' || expression[i] == '+')
         {
-            oper.push_back(expression[i]);
-            number.push_back(atoi(num.c_str()));
-            num="";
+            op.push_back(expression[i]);
+            num.push_back(stoi(temp));
+            temp = "";
         }
-        else    num+= expression[i];  
-    }
-    number.push_back(atoi(num.c_str()));
-    long long _max =0;
-    do{
-        vector <char> tmp_oper = oper;
-        vector <long long> tmp_num = number;
-        for(int i=0;i<3;i++)
+        else
         {
-            for(int j=0;j<tmp_oper.size();j++)
+            temp += expression[i];
+        }
+    }
+    num.push_back(stoi(temp));
+    long long Max = 0;
+    do{
+        vector<long long> tmp_num = num;
+        vector<char> tmp_op = op;
+        for(int i=0; i < oper.size(); i++)
+        {
+            for(int j=0; j < tmp_op.size(); j++)
             {
-                if(tmp_oper[j] == oper_list[i])
+                if(oper[i]==tmp_op[j])
                 {
-                    tmp_num[j] = calc(tmp_num[j],tmp_num[j+1],oper_list[i]);
+                    tmp_num[j] = calc(tmp_num[j],tmp_num[j+1],tmp_op[j]);
+                    tmp_op.erase(tmp_op.begin() + j);
                     tmp_num.erase(tmp_num.begin()+j+1);
-                    tmp_oper.erase(tmp_oper.begin()+j);
                     j--;
                 }
             }
-        } 
-        if(_max < abs(tmp_num[0]))   _max = abs(tmp_num[0]);
-    }while(next_permutation(oper_list.begin(),oper_list.end()));
-    answer = _max;
+            if(Max < abs(tmp_num[0])) Max = abs(tmp_num[0]);
+        }
+        
+    }while(next_permutation(oper.begin(),oper.end()));
+    answer = Max;
     return answer;
 }
