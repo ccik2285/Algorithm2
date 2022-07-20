2792fbd9dcbf7cadfb46d50bf1ebd5a091a38f0a
lch <ccik1653@gmail.com>
2022-07-20 12:59:33 +0900

    .

---
+++ 백준/2170.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<pair<int,int>> dot;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int a,b;
+        cin >> a >> b;
+        dot.push_back({a,b});
+    }
+    sort(dot.begin(),dot.end());
+    int from = dot[0].first;
+    int to = dot[0].second;
+    int ans = 0;
+    for(int i=1; i < dot.size(); i++){
+        if(to >= dot[i].first) {
+            to = max(to,dot[i].second);
+        }
+        else {
+            ans += to - from;
+            from = dot[i].first;
+            to = dot[i].second;
+        }
+        
+    }
+    ans += to-from;
+    cout << ans;
+
+}
