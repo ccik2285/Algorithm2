73e4184293b175ee542fffe9c06f44b7872ad641
lch <ccik1653@gmail.com>
2022-09-30 15:45:45 +0900

    .

--- 프로그래머스/입국심사.cpp
+++ 프로그래머스/입국심사.cpp
@@ -1,30 +1,33 @@
-#include <algorithm>
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
 long long solution(int n, vector<int> times) {
     long long answer = 0;
-    sort(times.begin(), times.end());
-
-    long long min = 1;
-    long long max = n * (long long)times.back();
-
-    while (min <= max) {
-
-        long long avg = (max + min) / 2;
-        long long tmp = 0;
-
-        for (int i = 0; i < times.size(); i++) {
-            tmp += (avg / (long long) times[i]);
+    sort(times.begin(),times.end());
+    
+    long long low = 0;
+    long long high =(long long) times[times.size()-1] * n;
+    long long mid;
+    while(low <= high){
+        long long people = 0;
+        mid = (low + high) / 2;
+        for(int i=0; i < times.size(); i++)
+        {
+            people+= (mid/times[i]);
         }
-
-        if (tmp >= n) {
-            max = avg - 1;
-            answer = avg;
+        if(people >= n){
+            if(mid <=high){
+                answer = mid;
+            } 
+            high = mid-1;
         }
-        else min = avg + 1;
+        else{
+            low = mid+1;
+        }
     }
+    answer = mid;
     return answer;
 }
