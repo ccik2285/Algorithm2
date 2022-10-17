18846363300bef4a70bcd72246abe3f5a0e11fec
lch <ccik1653@gmail.com>
2022-10-17 17:49:13 +0900

    .

---
+++ stl공부/1017.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int a, int b, int c, int d) {
+    int answer = 0;
+
+    
+
+    return answer;
+}
+
+int main() {
+    cout << solution(3,5,7,1);
+}
