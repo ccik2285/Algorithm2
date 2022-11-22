4247ca28ba24ce25e3e6b5bf19c2d63389fd953e
lch <ccik1653@gmail.com>
2022-11-22 15:13:54 +0900

    .

--- stl공부/1122.cpp
+++ stl공부/1122.cpp
@@ -4,30 +4,34 @@
 
 using namespace std;
 
-bool cmp(pair<int,int> vt1,pair<int,int> vt2){
-    return vt1.second < vt2.second;
+bool cmp(pair<int, int> vt1, pair<int, int> vt2) {
+	if (vt1.second == vt2.second) {
+		return vt1.first > vt2.first;
+	}
+	else return vt1.second < vt2.second;
+
 }
 
 int main() {
-    int t; cin >> t;
-    for(int i=0; i < t; i++){
-        vector<pair<int,int>> vt;
-        int n; cin >> n;
-        int ans = 0;
-        int result = 0;
-        for(int j=1; j <= n; j++){
-            int a; cin >> a;
-            result += a;
-            vt.push_back(make_pair(j,a));
-        }
-        sort(vt.begin(),vt.end(),cmp);
-        while(result < n*2){
-            int k = 0;
-            result -= vt[k].second;
-            result += max(vt[k].second+vt[k].first,vt[k].first);
-            k++;
-            ans++;
-        }
-        cout <<"answer : " << ans << endl;
-    }
+	int t; cin >> t;
+	for (int i = 0; i < t; i++) {
+		vector<pair<int, int>> vt;
+		int n; cin >> n;
+		int ans = 0;
+		int result = 0;
+		for (int j = 1; j <= n; j++) {
+			int a; cin >> a;
+			result += a;
+			vt.push_back(make_pair(j, a));
+		}
+		sort(vt.begin(), vt.end(), cmp);
+		int k = 0;
+		while (result < n * 2) {
+			result -= vt[k].second;
+			result += max(vt[k].second + vt[k].first, vt[k].first);
+			k++;
+			ans++;
+		}
+		cout << "#"<< i+1 << " "  << ans  << endl;
+	}
 }
