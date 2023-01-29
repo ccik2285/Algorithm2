925434002d8329ab880932cf26741215537eabbd
lch <ccik1653@gmail.com>
2023-01-29 14:48:29 +0900

    0129

---
+++ 백준/2163.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n,m; cin >> n >> m;
+    int r = n*m;
+
+    cout << r-1;
+
+}
