6005a64279c8fcff0f4932947a98fd3d85fb2f51
lch <ccik1653@gmail.com>
2023-02-05 15:46:23 +0900

    0205

--- stl공부/0205.cpp
+++ stl공부/0205.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 #include <typeinfo>
 using namespace std;
+vector<int> vt;
+vector<int> ans;
+
 
 int main() {
     int M; cin >> M;
     int fill = M;
     int flag = 0;
     int maxt;
+    long long int width = 0;
     while(flag==0){
         for(int i=2; i <= sqrt(M); i++){
             int g = i*i;
@@ -21,5 +26,27 @@ int main() {
             fill++;
         }
     }
+    int low = fill / (maxt*maxt);
+    while(maxt % 2 == 0){
+        maxt /= 2;
+        low *= 4;
+    }
+
+    vt.push_back(maxt);
+    vt.push_back(maxt);
+    vt.push_back(low);
+
+    do{
+        long long int bottom = vt[0] * vt[1];
+        long long int side = vt[1] * vt[2];
+        long long int temp = bottom * 2 + side * 4;
+        if(temp < width){
+            width = temp;
+            ans = vt;
+        }
+    }while(next_permutation(vt.begin(),vt.end()));
+    
+    for(int i=0; i < 3; i++) cout << vt[i] << " ";
+
     
 }
