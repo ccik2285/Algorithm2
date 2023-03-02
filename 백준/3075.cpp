fb860f985b45b563d3f3c70095b5823c4ab8cdba
lch <ccik1653@gmail.com>
2023-03-03 00:21:42 +0900

    0303

---
+++ 백준/3075.cpp
@@ -0,0 +1,65 @@
+#include <include>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+using lld = long long;
+
+const int INF = 0x3F3F3F3F;
+const lld LINF = 0x3F3F3F3F3F3F3F3F;
+
+int main() {
+	int T;
+	scanf("%d", &T);
+	while (T--) {
+		int n, p, q;
+		scanf("%d %d %d", &n, &p, &q);
+
+		vector<int> people(n);
+		for (auto& x : people) scanf("%d", &x);
+
+		vector<vector<lld>> D(p, vector<lld>(p, LINF));
+		while (q--) {
+			int i, j, d;
+			scanf("%d %d %d", &i, &j, &d);
+			i--, j--;
+			D[i][j] = min(D[i][j], d + 0LL);
+			D[j][i] = min(D[j][i], d + 0LL);
+		}
+
+		for (int i = 0; i < p; ++i) D[i][i] = 0;
+
+		for (int k = 0; k < p; ++k) {
+			for (int i = 0; i < p; ++i) { 
+				if (D[i][k] == LINF) continue;
+				for (int j = 0; j < p; ++j) {
+					if (D[k][j] == LINF) continue;
+					D[i][j] = min(D[i][j], D[i][k] + D[k][j]);
+				}
+			}
+		}
+
+		int planet;
+		lld ans = LINF;
+	
+		for (int i = 0; i < p; ++i) {
+			
+			lld dist = 0LL;
+			bool find = true;
+			for (auto& p : people) {
+				if (D[i][p - 1] >= INF) {
+					find = false;
+					break;
+				}
+				dist += D[i][p - 1] * D[i][p - 1];
+			}
+
+			if (find && ans > dist) {
+				ans = dist;
+				planet = i + 1;
+			}
+		}
+		printf("%d %lld\n", planet, ans);
+	}
+	return 0;
+}
